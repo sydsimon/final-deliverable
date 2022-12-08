4894849aab79296a0fee528134bf03e0e0869d6e
Sydney Simon <sydsimon@sas.upenn.edu>
2022-12-08 15:48:10 -0500

    vacations

--- router-deliverable/src/App.js
+++ router-deliverable/src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <h1 class="flexbox-item header">Budget Rescue</h1>
         <h2 class="flexbox-item header2">
         Welcome to the best budget tracker there is to save your money. 
-        Click on the invoice page to see who owes you money, and click on the expenses page
+        Click on the invoice page to how much it costs for each vacation spot, and click on the expenses page
         to see how much you spend monthly on different expenses.
         </h2>
       </div>
--- router-deliverable/src/data.js
+++ router-deliverable/src/data.js
@@ -1,30 +1,30 @@
 let invoices = [
     {
-      name: "Santa Monica",
+      name: "Playa del Carmen, Mexico",
       number: 1995,
       amount: "$10,800",
       due: "12/05/1995",
     },
     {
-      name: "Stankonia",
+      name: "Sydney, Australia",
       number: 2000,
       amount: "$8,000",
       due: "10/31/2000",
     },
     {
-      name: "Ocean Avenue",
+      name: "Belize",
       number: 2003,
       amount: "$9,500",
       due: "07/22/2003",
     },
     {
-      name: "Tubthumper",
+      name: "Bora Bora",
       number: 1997,
       amount: "$14,000",
       due: "09/01/1997",
     },
     {
-      name: "Wide Open Spaces",
+      name: "Niagara Falls",
       number: 1998,
       amount: "$4,600",
       due: "01/27/1998",
